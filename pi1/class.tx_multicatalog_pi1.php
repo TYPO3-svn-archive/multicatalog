caf3be92221fc74969d4ef5048e90052d148f8b1
phpprof <phpprof@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-01-18 11:05:51 +0000

    Detailed Code Documentation (The description of renderRecord() explains all the magic of this extension) Code Cleanups Added backlinkStdWrap property
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/multicatalog/trunk@28947 735d13b6-9817-0410-8766-e36946ffe9aa

--- pi1/class.tx_multicatalog_pi1.php
+++ pi1/class.tx_multicatalog_pi1.php
@@ -24,7 +24,18 @@
 /**
  * [CLASS/FUNCTION INDEX of SCRIPT]
  *
- * Hint: use extdeveval to insert/update function index above.
+ *
+ *
+ *   51: class tx_multicatalog_pi1 extends tslib_pibase
+ *   66:     function main($content, $conf)
+ *   98:     function pi_wrapInBaseClass($str)
+ *  109:     function singleView()
+ *  134:     function listView()
+ *  210:     function renderRecord()
+ *
+ * TOTAL FUNCTIONS: 5
+ * (This index is automatically created/updated by the extension "extdeveval")
+ *
  */
 
 require_once(PATH_tslib . 'class.tslib_pibase.php');
@@ -42,52 +53,66 @@ class tx_multicatalog_pi1 extends tslib_pibase {
 	var $scriptRelPath = 'pi1/class.tx_multicatalog_pi1.php';	// Path to this script relative to the extension dir.
 	var $extKey        = 'multicatalog';	// The extension key.
 	var $pi_checkCHash = true;
-	
+
 	private $uploadFolder = 'uploads/tx_multicatalog/';
-	
+
 	/**
 	 * Main method of your PlugIn
 	 *
-	 * @param	string		$content: The content of the PlugIn
-	 * @param	array		$conf: The PlugIn Configuration
-	 * @return	The content that should be displayed on the website
+	 * @param	string		The content of the PlugIn
+	 * @param	array		The PlugIn Configuration
+	 * @return	string		The content that should be displayed on the website
 	 */
 	function main($content, $conf)	{
 		$this->conf=$conf;
 		$this->pi_setPiVarDefaults();
 		$this->pi_loadLL();
-		$this->pi_USER_INT_obj=1;       
-		 
+		$this->pi_USER_INT_obj=1;
+
 		$this->pi_initPIflexForm();
-		
+
 		$this->view = $this->pi_getFFvalue($this->cObj->data['pi_flexform'], 'view', 'sDEF');
 		$this->listPid = $this->pi_getFFvalue($this->cObj->data['pi_flexform'], 'listPid', 'sDEF');
-		$this->singlePid = $this->pi_getFFvalue($this->cObj->data['pi_flexform'], 'singlePid', 'sDEF');		
-		
+		$this->singlePid = $this->pi_getFFvalue($this->cObj->data['pi_flexform'], 'singlePid', 'sDEF');
+
 		$this->template = $this->cObj->fileResource($this->conf['template']);
 		$this->articletemplate = $this->cObj->getSubpart($this->template, '###ARTICLE###');
-		
+
 		if($this->view == 'single'){
 			$content = $this->singleView();
 		}else{
 			$this->view = 'list';
 			$content = $this->listView();
 		}
-		
+
 		return $this->pi_wrapInBaseClass($content);
 	}
-	
+
+	/**
+	 * Custom implementation of tslib_pibase::pi_wrapInBaseClass
+	 * Adds the current view as class
+	 *
+	 * @param	string		Content to Wrap
+	 * @return	string		Content wrapped by div with Plugin Classes
+	 */
 	function pi_wrapInBaseClass($str){
 		return '<div class="'.str_replace('_','-',$this->prefixId).' '.str_replace('_','-',$this->prefixId).'-'.$this->view.'">' . $str . '</div>';
 	}
-	
+
+	/**
+	 * Single View
+	 * Makes a 301 redirect if no valid record for single view is available.
+	 * Uses $this->renderRecord() to get the Content of the single record
+	 *
+	 * @return	string		The Rendered View, ready for output
+	 */
 	function singleView(){
-		
+
 		$this->recordtemplate = $this->cObj->getSubpart(
 			$this->template,
 			'###RECORD_SINGLE###'
 		);
-		
+
 		$res = $GLOBALS['TYPO3_DB']->exec_SELECTquery('*', 'tx_multicatalog_catalog', 'uid = '.intval($this->piVars['uid']));
 		$this->record = $GLOBALS['TYPO3_DB']->sql_fetch_assoc($res);
 		if(!$this->record['uid']){
@@ -97,37 +122,95 @@ class tx_multicatalog_pi1 extends tslib_pibase {
 		}
 		$content = $this->renderRecord();
 		return $content;
-		
+
 	}
-	
+
+	/**
+	 * List View
+	 * Uses $this->renderRecord() to get the Contents of the records
+	 *
+	 * @return	string		The Rendered View, ready for output
+	 */
 	function listView() {
-		
+
 		$this->recordtemplate = $this->cObj->getSubpart(
 			$this->template,
 			'###RECORD_LIST###'
 		);
-		
+
 		$markerArray = array();
-		
+
 		$this->pids = $this->pi_getPidList($this->cObj->data['pages'],$this->cObj->data['recursive']);
 		$where = 'deleted = 0 AND hidden = 0 AND sys_language_uid = '.intval($GLOBALS['TSFE']->sys_language_content).' AND pid IN ('.$this->pids.')';
-		
+
 		$res = $GLOBALS['TYPO3_DB']->exec_SELECTquery('*', 'tx_multicatalog_catalog', $where, '', 'sorting ASC');
 		while($this->record = $GLOBALS['TYPO3_DB']->sql_fetch_assoc($res)){
 			$markerArray['###RECORDS###'] .= $this->renderRecord();
 		}
-		
+
 		return $this->cObj->substituteMarkerArray(
 			$this->cObj->getSubpart($this->template,'###LISTVIEW###'),
 			$markerArray
 		);
-		
+
 	}
-	
+
+	/**
+	 * Renders record content for single and list view
+	 * Offers some special flexibilities:
+	 *
+	 * All fields are available as markers:
+	 * ====================================
+	 * All fields in tx_multicatalog_catalog are available as markers for the template.
+	 * E.g. the uid is available as ###UID###. But also fields that come from other Extensions are available.
+	 * Assume you have a field tx_multicatalogdatasheet_sheet added by another extension, the field will be
+	 * available as ###TX_MULTICATALOGDATASHEET_SHEET###. That makes extening this extension very easy! Just
+	 * add the field to the database and you can instantly use it in the template.
+	 * If you're working with articles, the same rules apply for them.
+	 *
+	 * stdWrap for all fields/markers:
+	 * ===============================
+	 * For all fields and additional markers (see below) stdWrap properties are available. Just use the property named
+	 * <yourfield>StdWrap. (without the brackets). Example:
+	 * plugin.tx_multicatalog_pi1.list{
+	 *   descriptionStdWrap{
+	 *     crop = 160 | ... | 1
+	 *     stripHtml = 1
+	 *   }
+	 * }
+	 * If you work with articles, their fields are available below plugin.tx_multicatalog_pi1.[single/list].articles
+	 *
+	 * Other TS properties:
+	 * ====================
+	 * For all fields and additional markers (see below) a .link property is available which links the content to the
+	 * single view. Example: see "Add custom markers"
+	 * If you work with articles, their fields are available below plugin.tx_multicatalog_pi1.[single/list].articles
+	 *
+	 * Additional native markers:
+	 * =========================
+	 * ###FIRST_PICTURE### keeps only the first picture rendered. This is especially useful for the list view.
+	 * It automatically takes over the TS settings
+	 * ###BACKLINK### shows a link to list view. Wording is set by LLL:backlink
+	 *
+	 * Add custom markers:
+	 * ===================
+	 * You can also add markers via TS. Here's an example to add a "more" link to the list view:
+	 * plugin.tx_multicatalog_pi1.list{
+	 *   additionalMarkers{
+	 *     more = TEXT
+	 *     more.value = more
+	 *   }
+	 *   more.link = 1
+	 *   moreStdWrap = <span class="morelink">|</span>
+	 * }
+	 * If you work with articles, you can add fields below plugin.tx_multicatalog_pi1.[single/list].articles.additionalMarkers
+	 *
+	 * @return	string		The rendered record is given back to singleView() or listView()
+	 */
 	function renderRecord(){
 		$this->cObj->data = $this->record;
 		$markerArray = array();
-		
+
 		// render additional markers
 		if(is_array($this->conf[$this->view . '.']['additionalMarkers.'])) {
 			foreach($this->conf[$this->view . '.']['additionalMarkers.'] as $marker => $markerCobj) {
@@ -136,14 +219,14 @@ class tx_multicatalog_pi1 extends tslib_pibase {
 				}
 			}
 		}
-		
+
 		foreach($this->record as $field => $value) {
-			
+
 			// ###PRICE###
 			if ($field == 'price') {
 				$value = number_format($value, 2, ',', '.');
 			}
-			
+
 			// ###PICTURES### AND ###FIRST_PICTURE###
 			if($field == 'pictures'){
 				$pictures = t3lib_div::trimExplode(',', $value, 1);
@@ -158,7 +241,7 @@ class tx_multicatalog_pi1 extends tslib_pibase {
 						'width' => $this->conf[$this->view.'.']['pictures.']['lightbox_width']
 					)
 				);
-				
+
 				$value = '';
 				// ###PICTURES###
 				foreach($pictures as $picture){
@@ -178,37 +261,36 @@ class tx_multicatalog_pi1 extends tslib_pibase {
 					}
 					$value .= $image;
 				}
-				
+
+				// ###FIRST_PICTURE###
 					// link if value.link = 1
-				if ($this->conf[$this->view.'.']['firstPicture.']['link'] == 1) {
-					$this->conf[$this->view.'.']['firstPictureStdWrap.']['typolink.']['parameter'] = $this->singlePid;
-					$this->conf[$this->view.'.']['firstPictureStdWrap.']['typolink.']['additionalParams'] = '&'.$this->prefixId.'[uid]='.$this->record['uid'];
+				if($this->conf[$this->view . '.']['firstPicture.']['link'] == 1) {
+					$this->conf[$this->view . '.']['firstPictureStdWrap.']['typolink.']['parameter'] = $this->singlePid;
+					$this->conf[$this->view . '.']['firstPictureStdWrap.']['typolink.']['additionalParams'] = '&' . $this->prefixId . '[uid]='.$this->record['uid'];
 				}
-				
-				// ###FIRST_PICTURE 
 				$imgConf['file'] = $this->uploadFolder . $pictures[0];
 				$firstPicture = $this->cObj->IMAGE($imgConf);
 				$markerArray['###FIRST_PICTURE###'] = $this->cObj->stdWrap(
 					$firstPicture,
 					$this->conf[$this->view.'.']['firstPictureStdWrap.']
 				);
-				
+
 			}
-			
+
 			// link if value.link = 1
-			if($this->conf[$this->view.'.'][$field.'.']['link'] == 1){
-				$this->conf[$this->view.'.'][$field.'StdWrap.']['typolink.']['parameter'] = $this->singlePid;
-				$this->conf[$this->view.'.'][$field.'StdWrap.']['typolink.']['additionalParams'] = '&'.$this->prefixId.'[uid]='.$this->record['uid'];
+			if($this->conf[$this->view . '.'][$field . '.']['link'] == 1){
+				$this->conf[$this->view . '.'][$field . 'StdWrap.']['typolink.']['parameter'] = $this->singlePid;
+				$this->conf[$this->view . '.'][$field . 'StdWrap.']['typolink.']['additionalParams'] = '&' . $this->prefixId . '[uid]=' . $this->record['uid'];
 			}
-			
-			// stdWrap for each value 
+
+			// stdWrap for each value
 			$markerArray['###' . strtoupper($field) . '###'] = $this->cObj->stdWrap(
 				$value,
-				$this->conf[$this->view.'.'][$field.'StdWrap.']
+				$this->conf[$this->view . '.'][$field . 'StdWrap.']
 			);
-			
+
 		}
-		
+
 		// Articles
 		if ($this->record['articles']) {
 			$markerArray['###ARTICLES###'] = '';
@@ -216,30 +298,30 @@ class tx_multicatalog_pi1 extends tslib_pibase {
 			$i = 0;
 			$res = $GLOBALS['TYPO3_DB']->exec_SELECTquery('*', 'tx_multicatalog_article', 'irre_parentid = '.$this->record['uid'], '', 'sorting ASC');
 			while($article = $GLOBALS['TYPO3_DB']->sql_fetch_assoc($res)){
-				
+
 				// Fill cObj with article fields, product fields (prefixed with "product_") and the iteration number
 				$this->cObj->data = $article;
 				foreach($this->record as $field => $value){
 					$this->cObj->data['product_'.$field] = $value;
 				}
 				$this->cObj->data['i'] = $i;
-				
+
 					// render additional markers
 				foreach($this->conf[$this->view . '.']['articles.']['additionalMarkers.'] as $marker => $markerCobj){
 					if($marker{strlen($marker)-1} != '.'){
 						$article[$marker] = $this->cObj->cObjGetSingle($markerCobj, $this->conf[$this->view . '.']['articles.']['additionalMarkers.'][$marker . '.']);
 					}
 				}
-				
+
 				$subMarkerArray = array();
-				foreach($article as $field => $value){
-					
+				foreach($article as $field => $value) {
+
 					// ###PRICE###
 					if ($field == 'price') {
 						$value = number_format($value, 2, ',', '.');
 					}
-					
-					// stdWrap for each value 
+
+					// stdWrap for each value
 					$subMarkerArray['###' . strtoupper($field) . '###'] = $this->cObj->stdWrap(
 						$value,
 						$this->conf[$this->view.'.']['articles.'][$field.'StdWrap.']
@@ -249,48 +331,18 @@ class tx_multicatalog_pi1 extends tslib_pibase {
 				$i++;
 			}
 		}
-		
+
 		// Backlink
-		$markerArray['###BACKLINK###'] = $this->cObj->typoLink($this->pi_getLL('backlink'), array('parameter'=>$this->listPid));
-		
+		// TODO: Implement Backlink, Morelink and maybe First Picture as "additional markers" in the default TS setup.
+		// Would keep the code cleaner
+		$markerArray['###BACKLINK###'] = $this->cObj->stdWrap(
+			$this->cObj->typoLink($this->pi_getLL('backlink'), array('parameter'=>$this->listPid)),
+			$this->conf[$this->view.'.']['backlinkStdWrap.']
+		);
+
 		return $this->cObj->substituteMarkerArray($this->recordtemplate, $markerArray);
 	}
-	
-	/**
-	 * Build the WHERE Statement responsible for correct language handling
-	 * Taken from tt_news
-	 * 
-	 * @return string WHERE statement 
-	 */
-	function getLanguageWhere() {
-		$where = '';
-		$sys_language_content = $GLOBALS['TSFE']->sys_language_content;
-		if ($this->sys_language_mode == 'strict' && $sys_language_content) {
-			// sys_language_mode == 'strict': If a certain language is requested, select only news-records from the default
-			// language which have a translation. The translated articles will be overlayed later in the list or single function.
-			$tmpres = $this->exec_getQuery('tx_multicatalog_catalog', array(
-				'selectFields' => 'tx_multicatalog_catalog.l18n_parent',
-				'where' => 'tx_multicatalog_catalog.sys_language_uid = ' . $sys_language_content,
-				'pidInList' => $this->pids
-			));
 
-			$strictUids = array();
-			while (($tmprow = $GLOBALS['TYPO3_DB']->sql_fetch_assoc($tmpres))) {
-				$strictUids[] = $tmprow['l18n_parent'];
-			}
-			$strStrictUids = implode(',', $strictUids);
-			$where .= '(tt_news.uid IN (' . ($strStrictUids ? $strStrictUids : 0) . ') OR tt_news.sys_language_uid=-1)'; // sys_language_uid=-1 = [all languages]
-		} else {
-			// sys_language_mode NOT 'strict': If a certain language is requested, select only news-records in the default language.
-			// The translated articles (if they exist) will be overlayed later in the displayList or displaySingle function.
-			$where .= 'tt_news.sys_language_uid IN (0,-1)';
-		}
-
-		if ($this->conf['showNewsWithoutDefaultTranslation']) {
-			$where = '(' . $where . ' OR (tt_news.sys_language_uid=' . $sys_language_content . ' AND NOT tt_news.l18n_parent))';
-		}
-		return ' AND ' . $where;
-	}
 }
 
 
